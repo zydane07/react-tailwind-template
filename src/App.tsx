8d0198aae6a2f8682765bfdd542a7bd27443727a
zydane07 <muhammadzydane3@gmail.com>
2023-10-16 09:23:11 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="flex min-h-screen items-center justify-center p-10 font-bold">
+        <h1>hahah</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
